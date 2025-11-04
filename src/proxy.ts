38940ae51ba8cd180545b9d8f342a13049e0d40f
Babi4 <babich_dima3236@bk.ru>
2025-11-04 20:14:50 +0300

    update

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from 'next/server'
 import { headers } from 'next/headers'
 import { auth } from '@/shared/lib/auth'
 
+const publicRoute = ['/', '/login', 'signup']
+
 export async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -11,9 +13,15 @@ export async function proxy(request: NextRequest) {
   if (!session) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
+
+  // UPDATE REDIRECT PUBLIC PAGE
+  if (session && publicRoute.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard'], // Apply middleware to specific routes
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
