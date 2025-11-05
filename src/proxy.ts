394b23d7cf9a32de74d8f12b4751b3ad07a66fab
Babi4 <babich_dima3236@bk.ru>
2025-11-05 15:43:51 +0300

    update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,25 +3,33 @@ import type { NextRequest } from 'next/server'
 import { headers } from 'next/headers'
 import { auth } from '@/shared/lib/auth'
 
-const publicRoute = ['/', '/login', 'signup']
+const publicRoute = ['/', '/login', '/signup']
 
 export async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({
     headers: await headers(),
   })
 
-  if (!session) {
+  if (!session && !publicRoute.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  // UPDATE REDIRECT PUBLIC PAGE
   if (session && publicRoute.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
+    const lastPath = request.cookies.get('lastPath')?.value || 'dashboard'
+    return NextResponse.redirect(new URL(lastPath, request.url))
+  }
+
+  if (session) {
+    const response = NextResponse.next()
+    response.cookies.set('lastPath', request.nextUrl.pathname)
+    return response
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|avatars|.well-know).*)',
+  ],
 }
